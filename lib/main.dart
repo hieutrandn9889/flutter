e9f50680cdb912da95866a145bac27e72e7c22d1
Hieu Tran <hieutran@gai-goi.local>
2018-09-28 20:19:29 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
@@ -15,20 +14,48 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[]; // mảng chứa list các chữ random
+  final _suggestions = <WordPair>[];
+
   final _biggerFont = const TextStyle(fontSize: 18.0);
-  
-  
-  
-  
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          // add 1 cot
+          if (i.isOdd) return Divider();
+          // i ~/ 2 : example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            // lấy hết phần cuối available word 
+            _suggestions.addAll(generateWordPairs().take(10));
+            // show ra 10 cái và hơn
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
 
 class RandomWords extends StatefulWidget {
   @override
-  RandomWordsState createState() => RandomWordsState();
+  RandomWordsState createState() => new RandomWordsState();
 }
