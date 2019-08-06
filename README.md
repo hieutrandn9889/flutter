8fdcf52f2c0e74eb08062d2aecacdcdf2df18089
Hiếu Văn Trần <hieu.tran@enouvo.com>
2019-08-06 18:08:17 +0700

    update read me and main

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project_flutter_myapp
 flutter drive --target=test_driver/bdd.dart
+flutter run -d all
+flutter clean
+flutter packages pub cache clean
--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,49 @@
 import 'package:flutter/material.dart';
+void main() {
+ runApp(
+   MyApp()
+ );
+}
 
-void main() => runApp(MyApp());
+class MyAppBar extends StatelessWidget{
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // contructor
+  MyAppBar({this.title}); // de dua vao widget
+  final Widget title;
+  
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
+  Widget build(BuildContext context){
+    return Container(
+      height: 56.0, //float
+      padding: const EdgeInsets.symmetric(horizontal: 8.0), // padding 2 ben nam ngang 8.0
+      decoration: BoxDecoration(color: Colors.red),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "Navigation menu",
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: "Search",
+            onPressed: null,
+          )
+        ],
       ),
-      home: MyHomePage(title: 'ExecuteAutomation Home Page'),
     );
+    
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  void _showDialog() {
-    // flutter defined function
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // return object of type Dialog
-        return AlertDialog(
-          title: new Text("Alert message !"),
-          content: Text("Welcome to ExecuteAutomation " + _counter.toString(),
-              key: ValueKey("alert_text")),
-          actions: <Widget>[
-            // usually buttons at the bottom of the dialog
-            new FlatButton(
-              child: new Text(
-                "Close",
-                key: ValueKey("close_button"),
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title), key: ValueKey('app_bar')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display4,
-              key: ValueKey("counterText"),
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new RaisedButton(
-                  key: Key("add"),
-                  padding: const EdgeInsets.all(8.0),
-                  textColor: Colors.white,
-                  color: Colors.blue,
-                  onPressed: () {
-                    _showDialog();
-                  },
-                  child: new Text("Add"),
-                ),
-                new RaisedButton(
-                  key: Key("subtract"),
-                  onPressed: _decrementCounter,
-                  textColor: Colors.white,
-                  color: Colors.red,
-                  padding: const EdgeInsets.all(8.0),
-                  child: new Text(
-                    "Subtract",
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+class MyApp extends StatelessWidget{
+ @override
+ Widget build(BuildContext context){
+   return Center(
+     child: Text('Hello world112122', textDirection: TextDirection.ltr), // chu hien thi trai sang phai
+   );
+ }
 }
