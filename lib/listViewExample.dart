d5b560f6ecbda10464d2a15055204bacecdd4907
hieuTranEnouvo <hieu.tran@enouvo.com>
2019-10-06 15:49:01 +0700

    update Listview

--- lib/listViewExample.dart
+++ lib/listViewExample.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:project_flutter_myapp/flower.dart';
 import 'data.dart';
+import 'flower.dart';
 // quan ly state: StatefulWidget
 class ListViewExample extends StatefulWidget {
   @override
@@ -11,19 +13,31 @@ class ListViewExample extends StatefulWidget {
 }
 // khi state thay doi thi ham build dc goi va giao dien thay doi theo
 class ListViewExampleState extends State<ListViewExample> {
+  // thay doi mau khi tap vao trong GestureDetector
+  // selectedFlower: luu lai gia tri xem thu doi tuong hien tai hay k. Neu co thi chung ta se doi mau
+  var selectedFlower = new Flower();
+
+
   // mang cac object >> sao khi qua mang object do se thanh mang container
-  List<Container> _buildListItemsFromFlowers(){
+  //GestureDetector: click o chon vao se bien mau
+  List<GestureDetector> _buildListItemsFromFlowers(){
     // map: tao ra list doi tuong moi tu list doi tuong cu >> kieu doi tuong moi khac kieu doi tuong cu
     // list doi tuong flower >> doi tuong container >> anh xa
     int index = 0;
     return flowers.map((flower){
-      var container = Container(
-        // decoration: mau nen >> mau chia het cho 2 la chan 0xFFb0e0e6 nguoc lai 0xFF7ec0ee
-        decoration: index % 2 == 0?
+      var boxDecoration =index % 2 == 0?
         new BoxDecoration(color: const Color(0xFFb0e0e6)):
         new BoxDecoration(
             color: const Color(0xFF7ec0ee)
-        ),
+        );
+        if (selectedFlower == flower) {
+          boxDecoration = new BoxDecoration(
+            color: Colors.pinkAccent,
+          );
+        }
+      var container = Container(
+        // decoration: mau nen >> mau chia het cho 2 la chan 0xFFb0e0e6 nguoc lai 0xFF7ec0ee
+        decoration: boxDecoration,
         child: new Row(
           children: <Widget>[
             new Container(
@@ -70,7 +84,19 @@ class ListViewExampleState extends State<ListViewExample> {
         ),
       );
       index = index + 1;
-      return container;
+      // gestureDetector: tap vao textbox will show "flower.flowerName"
+      final gestureDetector = GestureDetector(
+        child: container,
+        onTap: (){
+          print("You tapped to ${flower.flowerName}");
+          // moi lan thay doi thi setState se thay doi
+          setState(() {
+            selectedFlower = flower;
+          });
+        },
+        
+      );
+      return gestureDetector;
     }).toList();
   }
   @override
