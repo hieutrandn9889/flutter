a1060a6dc84abc27a2f0aa0682bb7a59d6090c35
Hiếu Văn Trần <hieu.tran@enouvo.com>
2020-02-07 10:45:36 +0700

    update code POM

--- test_driver/pages/home_page.dart
+++ test_driver/pages/home_page.dart
@@ -1,29 +1,26 @@
 import 'package:flutter_driver/flutter_driver.dart';
+import '../common/basePage.dart';
+import '../interface/homaPageUI.dart';
 
-class HomePage {
-  final txtCounter = find.byValueKey('counterText');
-  final btnIncrement = find.byTooltip('Increment');
-  final btnAdd = find.byValueKey("add");
-  final btnSubtract = find.byValueKey("subtract");
-  final txtAlert = find.byValueKey("alert_text");
-  final btnClose = find.byValueKey("close_button");
-
+class HomePage extends BasePage {
+  
   FlutterDriver _driver;
+  final homePageUI = new HomePageUI();
 
   //Constructor
-  HomePage(FlutterDriver driver) {
+  HomePage(FlutterDriver driver) :super(driver) {
     this._driver = driver;
   }
 
   Future<String> getCounterValue() async {
-    return await _driver.getText(txtCounter);
+    return this.getTextElements(homePageUI.txtCounter);
   }
 
   Future<void> clickBtnPlus() async {
-    return await _driver.tap(btnIncrement);
+    return this.clickElements(homePageUI.btnIncrement);
   }
 
   Future<void> clickSubtractButton() async {
-    return _driver.tap(btnSubtract);
+    return this.clickElements(homePageUI.btnSubtract);
   }
 }
